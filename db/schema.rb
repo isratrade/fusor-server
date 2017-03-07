f276f8621a720d1227e7fad66de70905e17ea012
Joseph Magen <jmagen@redhat.com>
2017-03-07 22:28:53 +0800

    add subscriptions table, model, controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301184927) do
+ActiveRecord::Schema.define(version: 20170307125617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,6 +132,20 @@ ActiveRecord::Schema.define(version: 20170301184927) do
     t.datetime "created_at",                                       null: false
     t.datetime "updated_at",                                       null: false
     t.integer  "run_number",                       default: 0,     null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer  "deployment_id"
+    t.string   "contract_number",   limit: 255
+    t.string   "product_name",      limit: 255
+    t.integer  "quantity_attached"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer  "total_quantity"
+    t.string   "source",            limit: 255, default: "imported", null: false
+    t.integer  "quantity_to_add",               default: 0
   end
 
   add_foreign_key "deployment_delayed_jobs", "deployments"
